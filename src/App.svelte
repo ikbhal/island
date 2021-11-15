c98819a2e39cee90d795806ac17c9fc99caff6fd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 18:40:06 +0530

    Yos Sudarso Island Inodensia, papua province

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Island List</h1>
+	<p>List island, add, edit, list, search , delete</p>
+	<p>Why island list, research small group, will good names for program language</p>
+	<p>Yos Sudarso Island - https://en.wikipedia.org/wiki/Yos_Sudarso_Island - Indonesia island</p>
 </main>
 
 <style>
